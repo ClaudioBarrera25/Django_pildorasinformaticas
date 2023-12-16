f1d59218fdd7df0a8a9bf2e3712487225bb7ad9c
ClaudioBarrera25 <105759307+ClaudioBarrera25@users.noreply.github.com>
2023-12-16 11:44:51 -0300

    Contenido dinamico con fecha

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.views import saludo, despedida
+from Proyecto1.views import saludo, despedida, obtener_fecha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
-    path('chao/', despedida)
+    path('chao/', despedida),
+    path('fecha/',obtener_fecha)
 ]
--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+import datetime
 
 def saludo(request): # primera vista
-    return HttpResponse('Hola Alumnos.')
+    documento = '<html><body><h1>Hola Alumnos.</h1></body></html>'
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse('Hasta luego!')
+
+def obtener_fecha(request):
+    fecha_actual = datetime.datetime.now()
+    documento = f'<html><body><h1>fecha {fecha_actual}.</h1></body></html>'
+    return HttpResponse(documento)
